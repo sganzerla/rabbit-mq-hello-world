fbc5784d2dac885b042c4b20cadd3558b64c1a9d
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2021-06-25 23:20:25 -0300

    chore: start project

---
+++ ConsumerAmqp/Receive.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumerAmqp
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PublisherAmqp/Send.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublisherAmqp
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
